3779d2bee9ccedf681eea7e23915dbd2f3f494c1
Luke Andrews <Luke.Andrews@salesright.co>
2020-11-16 16:44:41 -0400

    Adding react router

---
+++ biovis/src/auth/login.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import * as React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <h2>hey there</h2>;
+  }
+}
+
+export default Login;
---
+++ biovis/src/web/dashboard.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import * as React from 'react';
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <h2>Dashboard</h2>;
+  }
+}
+
+export default Dashboard;
