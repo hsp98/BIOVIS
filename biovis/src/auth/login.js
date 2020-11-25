f81805de12323ad34f60c0d599cfe6a0640313f4
Luke Andrews <Luke.Andrews@salesright.co>
2020-11-25 12:57:27 -0400

    Updating sign up redirect

--- biovis/src/auth/login.js
+++ biovis/src/auth/login.js
@@ -69,7 +69,7 @@ class Login extends React.Component {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
