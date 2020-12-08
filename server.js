dc1d237475d5141e762691359e8b990d5776dd96
hsp98 <42342849+hsp98@users.noreply.github.com>
2020-12-07 23:03:38 -0400

    Deployment configurations added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/biovis/build/"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/biovis/build/index.html"));
+});
+
+app.listen(process.env.PORT || 5000, function () {
+  console.log("started ");
+});
